acaf18f947a8ce73e6551b317728e5b64a39f6a3
choisungwook <kgg1959@naver.com>
2020-10-27 17:51:24 +0900

    Add querydsl hello-world

---
+++ Readme.md
@@ -0,0 +1,31 @@
+# 1. QueryDsl 구현 과정
+1. queryFactory 초기화
+2. Q클래스 초기화
+3. 쿼리문 초기화
+```java
+@Test
+public void startQuerydsl(){
+    JPAQueryFactory jpaQueryFactory = new JPAQueryFactory(em);
+    QMember m = new QMember("m");
+
+    Member find_member = jpaQueryFactory
+            .select(m)
+            .from(m)
+            .where(m.username.eq("test23"))
+            .fetchOne();
+
+    assertThat(find_member.getUsername()).isEqualTo("test23");
+}
+```
+* 결과: jpql로 변환되서 쿼리문 실행
+```java
+select
+    member0_.member_id as member_i1_0_,
+    member0_.age as age2_0_,
+    member0_.team_id as team_id4_0_,
+    member0_.username as username3_0_ 
+from
+    member member0_ 
+where
+    member0_.username=?
+```
--- src/test/java/practice/querydsl/repository/MemberRepositoryTest.java
+++ src/test/java/practice/querydsl/repository/MemberRepositoryTest.java
@@ -1,5 +1,6 @@
 package practice.querydsl.repository;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -8,14 +9,19 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 import practice.querydsl.domain.Member;
+import practice.querydsl.domain.QMember;
 
+import javax.persistence.EntityManager;
 import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
 @Transactional(readOnly = true)
 public class MemberRepositoryTest {
     @Autowired MemberRepository memberRepository;
+    @Autowired EntityManager em;
 
     @Test
     @Transactional
@@ -38,8 +44,8 @@ public class MemberRepositoryTest {
                 () -> new IllegalStateException("존재하지 않은 계정")
         );
 
-        Assertions.assertThat(find_member.getUsername()).isEqualTo(new_member.getUsername());
-        Assertions.assertThat(find_member.getAge()).isEqualTo(new_member.getAge());
+        assertThat(find_member.getUsername()).isEqualTo(new_member.getUsername());
+        assertThat(find_member.getAge()).isEqualTo(new_member.getAge());
 
         return find_member;
     }
@@ -54,8 +60,17 @@ public class MemberRepositoryTest {
     }
 
     @Test
-    public void startJPQL(){
+    public void startQuerydsl(){
+        JPAQueryFactory jpaQueryFactory = new JPAQueryFactory(em);
+        QMember m = new QMember("m");
 
+        Member find_member = jpaQueryFactory
+                .select(m)
+                .from(m)
+                .where(m.username.eq("test23"))
+                .fetchOne();
+
+        assertThat(find_member.getUsername()).isEqualTo("test23");
     }
 
 }
